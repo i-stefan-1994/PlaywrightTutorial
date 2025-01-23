ca5e70a1e4dfc344044d0c33895968a7b79e2624
Stefan <istrate.stefan@protonmail.com>
2025-01-24 00:57:08 +0200

    24-01-2025 Finish Playwright tutorial

--- api_examples/tests/api_examples.spec.js
+++ api_examples/tests/api_examples.spec.js
@@ -23,4 +23,65 @@ test.describe("API Verification Examples", () => {
   expect(responseBody).toEqual(usersFixture)
   });
 
+  //2. test data for a single user line by line
+  test("Verify single user line by line", async ({request}) => {
+    const response = await request.get('https://reqres.in/api/users/1');
+    const responseBody = await response.json();
+    // console.log(responseBody)
+
+    //assert user information
+    expect(response.status()).toBe(200);
+    expect(responseBody.data.id).toEqual(1);
+    expect(responseBody.data.email).toEqual('george.bluth@reqres.in');
+    expect(responseBody.data.first_name).toEqual('George');
+    expect(responseBody.data.last_name).toEqual('Bluth');
+    expect(responseBody.data.avatar).toEqual('https://reqres.in/img/faces/1-image.jpg');
+  })
+
+  //3. POST request
+  test("Verify post request", async({request}) => {
+    const newUser = {
+      name: "Sam", 
+      job: "Cook"
+    };
+
+    //create post request
+    const response = await request.post('https://reqres.in/api/users', {data: newUser});
+    
+    //verify response code
+    expect(response.status()).toEqual(201)
+
+    const responseBody = await response.json();
+    //view response body
+    console.log(responseBody)
+
+    //verify response data
+    expect(responseBody.name).toBe(newUser.name);
+    expect(responseBody.job).toBe(newUser.job);
+  });
+
+  //4. PUT request
+  test('Verify PUT request', async ({request}) => {
+    const updatedUser = {
+      name: "Bob",
+      job: "Architect"
+    }
+
+    //do PUT request and save response
+    const response = await request.put('https://reqres.in/api/users/1', {data: updatedUser});
+    const responseBody = await response.json()
+    console.log(responseBody)
+
+    //verify the response
+    expect(response.status()).toBe(200);
+    expect(responseBody.name).toEqual(updatedUser.name);
+    expect(responseBody.job).toEqual(updatedUser.job)
+  });
+
+  //5. DELETE request
+  test('Verify user is deleted', async ({request}) => {
+    const response = await request.delete('https://reqres.in/api/users/1')
+
+    expect(response.status()).toBe(204);
+  });
 });
